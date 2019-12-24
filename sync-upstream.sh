33a00934f90add7e4a1584fe814de71a5fdf9ec2
Joao Antonio Ferreira <joao.parana@gmail.com>
2019-12-24 11:29:25 -0300

    Sheel para sincronizar com UpStream

--- sync-upstream.sh
+++ sync-upstream.sh
@@ -1,5 +1,8 @@
+echo "Fiz anteriormente: git remote add upstream git@github.com:microsoft/botbuilder-java.git"
 git fetch upstream
 git checkout master 
 git merge upstream/master
 echo "Repositorio sincronizado com upstream"
-echo "Teste, adicione, commit e faça push"
+echo "Agora você deve testar, adicionar, commitar e faça push"
+git status
+
